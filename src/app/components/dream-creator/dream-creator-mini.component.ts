2e128325db9335557ab7d26d1cb0441ec8275c7b
Natalia Bogutskaya <petajamaja@gmail.com>
2018-06-19 18:46:58 +0200

    Added empty chatbot page. Added asynchronous loading of CSV files with goals.

--- src/app/components/dream-creator/dream-creator-mini.component.ts
+++ src/app/components/dream-creator/dream-creator-mini.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ContentChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { DataService } from '../../services/data.service';
@@ -6,34 +6,94 @@ import Dream from '../../api/dream.interface';
 import { ModalService } from '../../services/modal.service';
 
 @Component({
-  selector: 'app-new-dream-mini',
-  templateUrl: './dream-creator-mini.component.html',
-  styleUrls: ['./dream-creator-mini.component.scss']
+    selector: 'app-new-dream-mini',
+    templateUrl: './dream-creator-mini.component.html',
+    styleUrls: ['./dream-creator-mini.component.scss', '../shared/modal.component.scss', 'upload.modal.component.scss']
 })
 export class DreamCreatorMiniComponent implements OnInit {
 
-  goals: any;
-  goalText : String;
-  defaultStatus = { text: "Haven't started yet", value: 0 };
+    goals: any;
+    goalText: String;
+    filesToUpload: Array<File>;
+    defaultStatus = { text: "Haven't started yet", value: 0 };
 
-  constructor(private _data: DataService, private _modal: ModalService) { 
-  }
+    constructor(private _data: DataService, private _modal: ModalService) {
+    }
 
-  ngOnInit() {
-    this._data.goal.subscribe(res => this.goals = res);
-    this._data.changeGoal(this.goals);
-  }
+    ngOnInit() {
+        this._data.goal.subscribe(res => this.goals = res);
+        this._data.changeGoal(this.goals);
+    }
 
-  openModal() {
-    this._modal.open("file-upload-modal");
-  }
+    openModal() {
+        this._modal.open("file-upload-modal");
+    }
 
-  addItem() {
-    this.goals.push({id: this.goals.length, name: this.goalText,
-                    description: '', tags: [], imageUrl: '',
-                    completionStatus: this.defaultStatus});
-    this.goalText = '';
-    this._data.changeGoal(this.goals);
-  }
+    addItem() {
+        this.goals.push({
+            id: this.goals.length, name: this.goalText,
+            description: '', tags: [], imageUrl: '',
+            completionStatus: this.defaultStatus
+        });
+        this.goalText = '';
+        this._data.changeGoal(this.goals);
+    }
+
+    toStatusObj(status){
+        switch(status){
+            case 100:
+                return { text: "Done with it", value: 100 };
+            case 90:
+                return { text: "Just a bit more", value: 90 };
+            case 75:
+                return { text: "Most of it done", value: 75 };
+            case 50:
+                return { text: "Halfway there", value: 50 };
+            case 25:
+                return { text: "Worked for a while", value: 25 };
+            case 10:
+                return { text: "Started recently", value: 10 };
+            case 0:
+                return { text: "Haven't started yet", value: 0 };
+            default:
+                return { text: "Haven't started yet", value: 0 }
+        }
+    }
+
+    addMoreGoals(newGoals: any){
+        this.goals = this.goals.concat(newGoals);
+    }
+
+    upload() {
+        var selectedFile = (<HTMLInputElement>document.getElementById('dreams-upload')).files[0];
+        var reader = new FileReader();
+        // here we add the new goals to goals array in service
+        //var that = this;
+        reader.onload = function () {
+            var content = reader.result;
+            var lines = content.split("\n");
+            var uploadedGoals = [];
+            for(var i=1; i < lines.length-1; i++){
+              var line = lines[i];
+              var fields = line.split(";");
+              var dream = {};
+              dream["id"] =  Number.parseInt(fields[0]) + this.goals.length;
+              dream["name"] = fields[1];
+              dream["description"] = fields[2];
+              dream["tags"] = (fields[3]==[])?[]:fields[3].split(",");
+              dream["imageUrl"] = fields[4];
+              dream["completionStatus"] = this.toStatusObj(fields[4]);
+              uploadedGoals.push(dream);
+            }
+            this.addMoreGoals(uploadedGoals);
+            this._data.changeGoal(this.goals);
+            this._modal.close('file-upload-modal');
+        }.bind(this);
+        reader.readAsText(selectedFile, 'utf8');       
+    }
+
+    fileChangeEvent(fileInput: any) {
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+    }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,13 +11,12 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss','../shared/modal.component.scss','./home.modal.component.scss'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
   itemCount: number;
   goals: Dream[] = [];
-  filesToUpload: Array<File>;
 
   constructor(private _data: DataService, private _modal : ModalService) { }
 
@@ -30,46 +29,4 @@ export class HomeComponent implements OnInit {
         $('#file-selected').html(fileName); 
     })
   }
-
-  openModal(id: string){
-    this._modal.open(id);
-  }
-
-  closeModal(id: string){
-    this._modal.close(id);
-  }
-
-  upload() {
-    this.makeFileRequest("http://localhost:3000/upload", [], this.filesToUpload).then((result) => {
-        console.log(result);
-    }, (error) => {
-        console.error(error);
-    });
-  }
-
-fileChangeEvent(fileInput: any){
-    this.filesToUpload = <Array<File>> fileInput.target.files;
-}
-
-makeFileRequest(url: string, params: Array<string>, files: Array<File>) {
-    return new Promise((resolve, reject) => {
-        var formData: any = new FormData();
-        var xhr = new XMLHttpRequest();
-        for(var i = 0; i < files.length; i++) {
-            formData.append("uploads[]", files[i], files[i].name);
-        }
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    resolve(JSON.parse(xhr.response));
-                } else {
-                    reject(xhr.response);
-                }
-            }
-        }
-        xhr.open("POST", url, true);
-        xhr.send(formData);
-    });
-}
-
 }
